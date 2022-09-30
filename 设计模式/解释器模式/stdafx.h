a932eeefc7af9316d05316938b269f51d5ad54d8
ThankVinci <thankvinci@163.com>
2022-09-30 15:28:00 +0800

    添加了一个强行解释的解释器，不是解释器模式，回去再改改

---
+++ 设计模式/解释器模式/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using std::cout;
+using std::string;
+using std::vector;
+using std::map;
+using std::pair;
